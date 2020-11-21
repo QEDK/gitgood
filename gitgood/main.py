735305b8802628909f691778fb34a6459a2f3a9a
Rohan Rout <rohan.rout98@gmail.com>
2020-11-22 00:58:47 +0530

    :sparkles: app: Add options to view projects and cards

--- gitgood/main.py
+++ gitgood/main.py
@@ -1,7 +1,9 @@
 import os
 import typer
+import github
 from dotenv import load_dotenv
 from pathlib import Path
+from github import Github
 
 app = typer.Typer()
 
@@ -32,13 +34,70 @@ except Exception:
         file.write(f"GG_PAT={access_token}")
     typer.secho("Your token has been stored successfully. 🚀", fg=typer.colors.GREEN, bold=True)
 
+g = Github(access_token)
 
 @app.callback()
 def callback():
     """
     Here to help! ✨
     """
+@app.command()
+def project(repo_address:str, show_projects:bool = False, show_cards: bool = False):
+    """This option deals with the repo projects
+
+    Args: repo_address (str): Should be in the form of "Username/Repository name", for ex: QEDK/gitgood
+        
+        \n\nshow-projects (bool, optional): Enabling this option allows you to view all the projects for the address of the repository provided. \n\nUsage: gg project --show-projects QEDK/gitgood (Defaults to False)
+        \n\nshow-cards (bool, optional): Enabling this option allows you to view all the cards for all the projects in the address of the repository provided. \n\nUsage: gg project --show-cards QEDK/gitgood (Defaults to False)
+    """
+    repo = g.get_repo(repo_address)
+    typer.echo("----------------------------------------")
+    count = 1
+    if show_projects:
+        for project in repo.get_projects():
+            project_name = project.name
+            typer.echo(f"{count}. {project_name}")
+            count+=1
+    
+    if show_cards:
+        show_card(repo_address)
+
+
+def show_card(repo_address:str):
+    repo = g.get_repo(repo_address)
+    typer.echo("----------------------------------------")
+    for project in repo.get_projects():
+        typer.echo(project.name)
+        typer.echo("------------")
+        for column in project.get_columns():
+            typer.echo(column.name)
+            typer.echo("------------")
+            for card in column.get_cards():
+                url = card.get_content().url
+                slash_count = 0
+                for ch in url:
+                    if slash_count == 6:
+                        break
+                    if ch == '/':
+                        slash_count += 1
+                if ch == 'p':
+                    typer.echo("Pull Request:")
+                elif ch == 'i':
+                    typer.echo("Issue:")
+                    if card.get_content().assignee == None:
+                        pass
+                    else:
+                        typer.echo(f"Assignee: {card.get_content().assignee.login}")
+                else:
+                    typer.echo("Note:")
+                typer.echo(card.get_content().title)
+                typer.echo("Labels:")
+                for label in card.get_content().labels:
+                        typer.echo(label.name)
+                typer.echo(card.get_content().body)
+                typer.echo("------------")
+            typer.echo("------------")
 
 
 if __name__ == "__main__":
-    typer.run(callback)
+    app()
