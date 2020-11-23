71cc53ae9c68c2e2285599c2a3ff2669cfc739e6
sakshi rambhia <sakshirambhia@Sakshis-MacBook-Pro.local>
2020-11-23 08:12:02 -0500

    bugs fixed.

--- notifs.py
+++ notifs.py
@@ -12,6 +12,7 @@ user = g.get_user()
 app = typer.Typer()
 
 
+@app.command()
 def notifs(limit: Optional[int] = typer.Argument(sys.maxsize)):
     today = date.today()
     message = ""
@@ -31,8 +32,8 @@ def read(limit: Optional[int] = typer.Argument(sys.maxsize)):
                                                  before=datetime(today.year, today.month, today.day))
     for notif in unread_notification[:limit]:
         notif.mark_as_read()
-        message += "Marked as Read \n"
-    typer.secho(f"Marked as Read.", fg=typer.colors.BRIGHT_CYAN)
+        message += "Marked as Read. \n"
+    typer.secho(f"{message}", fg=typer.colors.BRIGHT_CYAN)
 
 
 if __name__ == "__main__":
