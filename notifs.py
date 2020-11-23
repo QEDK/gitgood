304f9951cf0318e35f21f22abce45ad23ab65324
sakshi rambhia <sakshirambhia@Sakshis-MacBook-Pro.local>
2020-11-22 19:58:11 -0500

    typer integration added

--- notifs.py
+++ notifs.py
@@ -2,38 +2,43 @@ import base64
 from github import Github
 from pprint import pprint
 from datetime import datetime
+from datetime import date
 import typer
 
-username = {username}
-g = Github({token})
+username = "korusuke"
+g = Github("ed8c563691a672782c935d8f635e7b11e3744488")
 user = g.get_user(username)
+app = typer.Typer()
 
 
+@app.command()
 def notifs(limit: int):
     today = date.today()
     repos = user.get_repos(type="all", sort="updated", direction="asc")
     max = limit
     for repo in repos:
-        notifs = repo.get_notifications(all=False, participating=True, since=datetime(
-            2019, 11, 7), before=datetime(today.year, today.month, today.day))
+        notifs = repo.get_notifications(all=False, participating=True, since=datetime(2019, 11, 7),
+                                        before=datetime(today.year, today.month, today.day))
         if limit == 0:
             break
         for notif in notifs:
-            print(notif)
+            typer.echo(notif)
             limit -= 1
             if limit == 0:
                 break
 
 
+@app.command()
 def getAllNotifs():
     today = date.today()
     for repos in user.get_subscriptions():
-        notifs = repos.get_notifications(all=False, participating=True, since=datetime(
-            2019, 11, 7), before=datetime(2020, 11, 21))
+        notifs = repos.get_notifications(all=False, participating=True, since=datetime(2019, 11, 7),
+                                         before=datetime(2020, 11, 21))
         for notif in notifs:
-            print(notif)
+            typer.echo(notif)
 
 
+@app.command()
 def markAsRead(num: int):
     today = date.today()
     repos = user.get_repos(type="all", sort="updated", direction="asc")
@@ -45,21 +50,11 @@ def markAsRead(num: int):
             break
 
 
+@app.command()
 def markRepoAsRead(repo_name):
     repo = g.get_repo(repo_name)
     repo.mark_notifications_as_read()
 
-# app = typer.Typer()
 
-
-# @app.command()
-# def getNotifs():
-#     for repos in user.get_subscriptions():
-#         notifs = repos.get_notifications(all= False, participating= True, since = datetime(2019,11,7), before= datetime(2020,11,21))
-#     for notif in notifs[:10]:
-#         print(notif)
-#         typer.secho(notif, fg=typer.colors.MAGENTA)
-
-
-# if __name__ == "__main__":
-#     app()
+if __name__ == "__main__":
+    app()
